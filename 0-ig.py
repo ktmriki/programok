018f7da261b36c8dbe5b8d926d888b198998b50e
ktmriki <31551924+ktmriki@users.noreply.github.com>
2017-10-16 08:25:07 +0200

    Update 0-ig.py

--- 0-ig.py
+++ 0-ig.py
@@ -1,10 +1,10 @@
-n = int(input("Szám: "))
-x = []
-y = int(0)
-z = int(0)
-zs = int(0)
+n = int(input("Szám: "))                #Bemenet
+x = []                                  #Lista
+y = int(0)                              #Összeg
+z = int(0)                              #Pozitív számok száma
+zs = int(0)                             #Negatív számok száma
 
-while n != 0:                       #A számok listája
+while n != 0:                           #A számok listája
     x.append(n)
     n = int(input("Szám: "))
 
@@ -12,20 +12,19 @@ x.append(0)
 
 print ("A számok listája: ",x)
 
-for element in list(x):             #A számok összege
+for element in list(x):                 #A számok összege
     y = y + element
 
 print ("A számok összege: ",y)
 
-for element in list(x):
+for element in list(x):                 #A pozitív számok száma
     if element > 0:
         z = z + 1
 
 print ("A pozitív számok száma: ",z)
 
-for element in list(x):
+for element in list(x):                 #A negatív számok száma
     if element < 0:
         zs = zs + 1
 
 print ("A negatív számok száma: ",zs)
-
