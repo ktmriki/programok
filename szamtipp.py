d623e45562290fa73f4153b1c91e7e1e959d6798
ktmriki <31551924+ktmriki@users.noreply.github.com>
2017-12-11 08:45:23 +0100

    Update szamtipp.py

--- szamtipp.py
+++ szamtipp.py
@@ -1,6 +1,8 @@
 import random
 
-x = int(random.randint(0, 100))
+x = int(random.randint(-50, 100))
+
+i = 0 #Hány tipp volt eddig
 
 print("Egy véletlen számot kell kitalálnod 0 és 100 között.")
 y = int(input("Tipped: "))
@@ -8,14 +10,17 @@ y = int(input("Tipped: "))
 while x != y:
     if x > y:
         print()
-        print("A tipped kisebb a számnál.")
+        print("A tipped kisebb a számnál. Tippelj nagyobbat.")
         print()
         y = int(input("Új tipped: "))
 
     elif x < y:
         print()
-        print("A tipped nagyobb a számnál.")
+        print("A tipped nagyobb a számnál. Tippelj kisebbet.")
         print()
         y = int(input("Új tipped: "))
 
-print ("Eltaláltad a számot!")
+    i = i + 1
+
+print()
+print ("Eltaláltad a számot", i, "tippel.")
